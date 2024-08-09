f2ad3fc9b5e076f63af67ccf96e99e8435e34367
vsmaliakou <vsmaliakou@yandex.by>
2024-08-09 15:28:27 +0300

    feat: Working with databases (Prisma)

--- src/flowers/flowers.module.ts
+++ src/flowers/flowers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
 import { FlowersController } from './flowers.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [FlowersController],
-  providers: [FlowersService],
+  providers: [FlowersService, PrismaService],
 })
 export class FlowersModule {}
--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,24 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class FlowersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   findAll() {
-    return [
-      {
-        name: 'Rose',
-        color: 'red',
-        price: 5,
-      },
-      {
-        name: 'Lily',
-        color: 'white',
-        price: 6,
-      },
-      {
-        name: 'Tulip',
-        color: 'yellow',
-        price: 7,
-      },
-    ];
+    return this.prisma.flower.findMany();
   }
 }
