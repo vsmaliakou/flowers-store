d54759fac9943f5924452a81fdf7e513a2f1bdfa
vsmaliakou <vsmaliakou@yandex.by>
2024-08-09 13:00:33 +0300

    feat: Decorators, Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { FlowersModule } from './flowers/flowers.module';
+import { LoggerMiddleware } from './conception/middleware';
 
 @Module({
   imports: [FlowersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('flowers');
+  }
+}
